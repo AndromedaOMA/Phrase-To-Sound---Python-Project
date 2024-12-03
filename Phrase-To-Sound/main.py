387823dfcbd98c05d6b249a202317f7d81e38f04
Olaru Marius-Alexandru <marius.alexandru.olaru@gmail.com>
2024-12-03 11:06:18 +0200

    first commit

---
+++ Phrase-To-Sound/main.py
@@ -0,0 +1,10 @@
+from gtts import gTTS
+from playsound import playsound
+
+test_text = ("Learn from yesterday, live for today, hope for tomorrow. "
+             "The important thing is not to stop questioning. - Einstein")
+language = "en"
+
+obj = gTTS(text=test_text, lang=language, slow=True)
+
+obj.save("test_sound.mp3")
